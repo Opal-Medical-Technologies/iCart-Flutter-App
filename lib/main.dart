17d5710dc436e1a7fb4ed536983ffe9fdaa544cb
Runxuan Jiang <runxuan.jiang@gmail.com>
2019-11-24 00:42:28 -0500

    Made progress on iCart app -> finished weight screen frontend and backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,345 +1,176 @@
-
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-
-
-class DrugState extends State<Drug> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: Column(
-        children: [
-          Text("Epinephrine"),//change to generic drug input
-          Text("2mg/100ml in DSW/NS (20mcg/ml"), //change to generic text
-          Expanded(
-            child: Row(
-              children:[
-                Column(
-                  children:[
-                    Text("DOSE (mcg/kg/min)"),
-                    Row(
-                      children:[
-                        Column(
-                          children:[
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            ),
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            )
-                          ]
-                        ),
-                        Column(
-                          children:[
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            ),
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            )
-                          ]
-                        ),
-                        Column(
-                          children:[
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            ),
-                            Chip(
-                              avatar: CircleAvatar(
-                                backgroundColor: Colors.grey.shade800
-                              ),
-                              label: Text("0.1")
-                            )
-                          ]
-                        )
-                      ]
-                    )
-                  ]
-                ),
-                Column(
-                  children:[
-                    Text("Rate (ml/hour)"),
-                    Text("0.9")
-                  ]
-
-                )
-              ]
-            )
-          )
-        ]
-      )
-    );
-  }
-}
-
-class Drug extends StatefulWidget {
-  @override
-  DrugState createState() => DrugState();
-}
-
-class TimeLineState extends State<TimeLine> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child:Column(
-        children:[
-          Text("TIMELINE"),
-          ListView(
-            children: [
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              ),
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              ),
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              ),
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              ),
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              ),
-              Row(
-                children:[
-                  Column(
-                    children:[
-                      Text(
-                        "Epinephrine",//replace with generic drug name
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                      Row(
-                        children:[
-                          Text("12:32 am"), //change to generic time
-                          Text("20 mg") //change to generic dosage
-                        ]
-                      )
-                    ]
-                  ),
-                  IconButton(
-                    //TODO implement later
-                  )
-                ]
-              )
-            ]
-          )
-        ]
-      )
-    );
-  }
-}
-
-class TimeLine extends StatefulWidget {
-  @override
-  TimeLineState createState() => TimeLineState();
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //bottom of screen
-    Widget bottom = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Chip(
-            avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800
-            ),
-            label: Text("10:32 am")
-          ),
-          Chip(
-            avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800
-            ),
-            label: Text("Defibrillation (2 J/kg):")
-          ),
-          Chip(
-            avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800
-            ),
-            label: Text("90J")
-          ),
-          Chip(
-            avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800
-            ),
-            label: Text("Cardioversion (Synchronized) (0.5 J/kg)")
-          ),
-          Chip(
-            avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800
-            ),
-            label: Text("22.5 J")
-          )
-        ]
-      )
-    )
-    //top center of screen
-    Widget topcenter = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Atropine',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'MAX TOTAL DOSE OF 1mg',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Text('38kg'),
-          Text('10 yrs'),
-        ],
-      ),
-    );
+    return WeightScreen();
+  }
+}
+
+/*class WeightButton {
+  FlatButton button;
+  int weight;
+  int id;
+
+  WeightButton(FlatButton ib, int w, int i) {
+    button = ib;
+    weight = w;
+    id = i;
+  }
+}*/
+
+class WeightScreen extends StatefulWidget {
+  @override
+  _WeightScreenState createState() => _WeightScreenState();
+}
+
+class _WeightScreenState extends State<WeightScreen> {
+  Container weightButton(int weight1, int weight2) {
+    if (weight2 == -1) {
+      return Container(
+          height: 120,
+          width: 150,
+          padding: EdgeInsets.all(10),
+          child: FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                setState(() {
+                  weightText =
+                    Text("Weight: $weight1 kg", style: TextStyle(fontSize: 40));
+                  weight = weight1;
+                  next = true;
+                });
+
+              },
+              child: Text(
+                "$weight1 kg",
+                style: TextStyle(fontSize: 20.0),
+              )));
+    } else {
+      return Container(
+          height: 120,
+          width: 150,
+          padding: EdgeInsets.all(10),
+          child: FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                setState(() {
+                  weightText =
+                    Text("Weight: $weight1 kg", style: TextStyle(fontSize: 40));
+                  weight = weight1;
+                  next = true;
+                });
+
+              },
+              child: Text(
+                "$weight1-$weight2 kg",
+                style: TextStyle(fontSize: 20.0),
+              )));
+    }
+  }
+
+  void proceed() {
+
+  }
+
+
+  bool next;
+  int weight;
+  Text weightText =
+      Text("Please select a weight", style: TextStyle(fontSize: 40));
+
+  List<Widget> weightButtons = [];
+
+  _WeightScreenState() {
+    weightButtons.add(weightButton(3, -1));
+    weightButtons.add(weightButton(4, -1));
+    weightButtons.add(weightButton(5, -1));
+    weightButtons.add(weightButton(6, 7));
+    weightButtons.add(weightButton(8, 9));
+    weightButtons.add(weightButton(10, 11));
+    weightButtons.add(weightButton(12, 14));
+    weightButtons.add(weightButton(15, 18));
+    weightButtons.add(weightButton(19, 23));
+    weightButtons.add(weightButton(24, 29));
+    weightButtons.add(weightButton(30, 36));
+    weightButtons.add(weightButton(37, -1));
+    next = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(weightButtons.length);
     return MaterialApp(
-      title: 'iCart',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('iCart App - version 1.2'),
-        ),
-          body: Column(
-            children: [
-              topcenter
-            ],
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+                title: Center(
+                    child: Text(
+              "Weight",
+              textAlign: TextAlign.center,
+            ))),
+            body: Container(
+                child: Stack(
+                  children: [Positioned(
+                    top:15.0,
+                    right:15.0,
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        color: next ? Colors.blue: Colors.grey,
+                        textColor: Colors.white,
+                        onPressed: next ? proceed: null,
+                        child: Text(
+                          "Next",
+                          style: TextStyle(fontSize: 40)
+                        )
+                      )
+                    )
+                  ),
+                    Column(children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: weightText,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              weightButtons[0],
+                              weightButtons[1],
+                              weightButtons[2],
+                              weightButtons[3],
+                            ])),
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              weightButtons[4],
+                              weightButtons[5],
+                              weightButtons[6],
+                              weightButtons[7],
+                            ])),
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              weightButtons[8],
+                              weightButtons[9],
+                              weightButtons[10],
+                              weightButtons[11],
+                            ])),
+                  ])
+                ])
+                  ])
+                )));
   }
 }
